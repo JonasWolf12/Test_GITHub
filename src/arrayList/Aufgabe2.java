29c35c5fbc838f7b146f3b5117220ae3e9685310
JonasWolf12 <Jonas.Wolf@hak-reutte.ac.at>
2024-03-11 15:24:10 +0100

    Aufgabe 2 arrayList (nicht fertig)

---
+++ src/arrayList/Aufgabe2.java
@@ -0,0 +1,16 @@
+package arrayList;
+
+import java.util.List;
+
+public class Aufgabe2 {
+
+	public static void main(String[] args) {
+
+		
+	}
+	
+	public void trimNonGrowingNumbers(List<Double>numbers) {
+		for(int i = 0; i < numbers.size() )
+	}
+
+}
