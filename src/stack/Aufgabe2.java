ba8054f046d9042a71cadbbfb1a29847e355ce1f
JonasWolf12 <Jonas.Wolf@hak-reutte.ac.at>
2024-04-16 12:15:40 +0200

    Aufgabe 2

--- src/stack/Aufgabe2.java
+++ src/stack/Aufgabe2.java
@@ -1,14 +1,49 @@
 package stack;
-
+ 
+import java.util.LinkedList;
+import java.util.regex.Pattern;
+ 
 public class Aufgabe2 {
+ 
+	/*
+	 * - Entwickle eine Anwendung, die überprüft, ob Klammern in einem gegebenen Ausdruck
+	 *   korrekt geschachtelt sind. Verwende dazu einen Stack, um die öffnenden und schließenden
+	 *   Klammern zu verfolgen.
+	 * - Die Anwendung sollte "Korrekte Klammerung!" ausgeben, wenn die
+	 *   Klammern korrekt geschachtelt sind, und "Fehlerhafte Klammerung!" sonst.
+	 */
+	
+	private static final Pattern klammern = Pattern.compile(
+			"(|)|[|]|{|}"
+		);
 	
 	
-	private static boolean 
-
 	public static void main(String[] args) {
-
 		
 		
+ 
 	}
-
+	
+	
+	private static boolean sameNumberOfClosedBrackets(String input) {
+		LinkedList<Character> brackets = new LinkedList<>();
+		char bracketNow ="";
+        for (int i = 0; i < input.length(); i++) {
+        	if(input.matches(klammern)) {
+        		brackets.push(input.charAt(i));
+        		char bracketNow ="";
+        	}
+        	if(brackets.getLast().equals(bracketNow)) {
+        		brackets.pop();
+        	}
+        	
+        }
+        
+        String reversed = "";
+        while (!brackets.isEmpty()) {
+        	reversed += brackets.pop();
+        }
+		return ;		
+	}
+ 
 }
